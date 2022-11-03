6274516311f28edf98775938f504f98c7e8bd361
Felipe Huerta Aros <hueeerta@gmail.com>
2022-11-03 12:32:44 -0300

    Se crean las funciones en el front para interactuar con el contrato inteligente

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,17 +2,35 @@ import "regenerator-runtime/runtime";
 import React, { useState, useEffect } from "react";
 
 const App = ({ isSignedIn, wallet, buskerManager }) => {
-  const [contractMessage, setContractMessage] = useState("loading...");
+  const [buskerList, setBuskerList] = useState([]);
+  const [newBusker, setNewBusker] = useState({ name: "" });
 
-  useEffect(() => {
-    buskerManager.getBuskers()
+  const getBuskersList = () => {
+    buskerManager
+      .getBuskers()
       .then((response) => {
         console.log(response);
-        setContractMessage(response);
+        setBuskerList(response);
       })
       .catch((error) => {
         console.error(error);
       });
+  }
+
+  const createNewBusker = (name) => {
+    buskerManager
+      .setBusker(name)
+      .then((response) => {
+        console.log(response);
+        getBuskersList();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  useEffect(() => {
+    getBuskersList();
   }, []);
 
   if (!isSignedIn) {
@@ -30,13 +48,64 @@ const App = ({ isSignedIn, wallet, buskerManager }) => {
     );
   }
 
+  const handleInputChange = (event) => {
+    const buskerInput = { ...newBusker };
+    if (event.target.id === "name") {
+      buskerInput.name = event.target.value;
+    }
+    setNewBusker(buskerInput);
+  };
+
+  const handleNewBusker = (event) => {
+    event.preventDefault();
+    console.log("Calling set_busker() on chain:", newBusker);
+    createNewBusker(newBusker.name); 
+  };
+
   return (
     <>
+      <h1>Busker Donation Plataform</h1>
       <p>Welcome</p>
       <button type="button" onClick={() => wallet.signOut()}>
         Log out {wallet.accountId}
       </button>
-      <p>Message: {contractMessage}</p>
+      <hr />
+      <form>
+        <h3>Create your Busker Profile here:</h3>
+        <label htmlFor="name">
+          Busker Name:
+          <input
+            id="name"
+            name="name"
+            type="text"
+            value={newBusker.name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="submit" onClick={handleNewBusker}>
+          Create my Busker profile
+        </button>
+      </form>
+      <hr />
+      <h3>Search for a Buskert to donate to:</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>ACCOUNT</th>
+            <th>NAME</th>
+          </tr>
+        </thead>
+        <tbody>
+          {buskerList.map((busker) => (
+            <tr key={busker.id}>
+              <td>{busker.id}</td>
+              <td>{busker.account_id}</td>
+              <td>{busker.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
--- frontend/lib/busker-manager.js
+++ frontend/lib/busker-manager.js
@@ -4,6 +4,14 @@ export class BuskerManager {
     this.wallet = userWallet;
   }
 
+  async setBusker(name) {
+    return await this.wallet.callMethod({
+      method: "set_busker",
+      args: { name: name },
+      contractId: this.contractId,
+    });
+  }
+
   async getBuskers() {
     return await this.wallet.viewMethod({
       contractId: this.contractId,
